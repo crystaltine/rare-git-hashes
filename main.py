494c79a880d9230abc6d4a7b10011d03cac3df46
crystaltine <msh379c@outlook.com>
2025-07-18 09:20:32 -0700

    add topk functionality + display improvements!

--- main.py
+++ main.py
@@ -41,90 +41,130 @@ def get_commit_info(_hash: str) -> dict:
 		return info
 
 	except subprocess.CalledProcessError as e:
-		print(f"\x1b[31mError while getting commit info for {_hash}: {e}")
+		print(f"\x1b[31merror: {e}\x1b[0m")
 		exit(1)
 
-# (len 42)
-# 0 letters or more -> 41 letters or more, hardcoded cuz idgaf
-PROBS = [1, 0.9999999931577225, 0.9999998289430587, 0.9999979076314924, 0.9999833056635884, 0.999902264741721, 0.9995521679592538, 0.9983268292206189, 0.9947558420394541, 0.9859176487660712, 0.9670628364495212, 0.931992885540738, 0.8746056931445474, 0.7913942641700711, 0.6838594944184401, 0.5594264037058385, 0.4300159893647329, 0.3086937259199464, 0.20592663217848023, 0.12713852697668945, 0.07240152757334008, 0.037917217949229975, 0.018211898164024208, 0.008000959729872126, 0.0032062582042702795, 0.0011685100558894946, 0.00038601476691127326, 0.00011515101303419666, 3.088228960577282e-05, 7.407430936426176e-06, 1.5791901633470091e-06, 2.9697719326959257e-07, 4.880694099654422e-08, 6.928210925467301e-09, 8.367592787652037e-10, 8.428584005494473e-11, 6.888572073318082e-12, 4.3879974151586167e-13, 2.0436130804366296e-14, 6.18907139084941e-16, 9.14641092243755e-18, 0]
+def ellipsisize_text(text: str, max_len: int = 50) -> str:
+	""" returns text truncated to max_len with ellipsis (...) if it exceeds `max_len` """
+	if len(text) > max_len:
+		return text[:max_len - 3] + '...'
+	return text
 
+# (len 41)
+# 0 letters or more -> 40 letters or more, hardcoded cuz idgaf
+PROBS = [1, 0.9999999931577225, 0.9999998289430587, 0.9999979076314924, 0.9999833056635884, 0.999902264741721, 0.9995521679592538, 0.9983268292206189, 0.9947558420394541, 0.9859176487660712, 0.9670628364495212, 0.931992885540738, 0.8746056931445474, 0.7913942641700711, 0.6838594944184401, 0.5594264037058385, 0.4300159893647329, 0.3086937259199464, 0.20592663217848023, 0.12713852697668945, 0.07240152757334008, 0.037917217949229975, 0.018211898164024208, 0.008000959729872126, 0.0032062582042702795, 0.0011685100558894946, 0.00038601476691127326, 0.00011515101303419666, 3.088228960577282e-05, 7.407430936426176e-06, 1.5791901633470091e-06, 2.9697719326959257e-07, 4.880694099654422e-08, 6.928210925467301e-09, 8.367592787652037e-10, 8.428584005494473e-11, 6.888572073318082e-12, 4.3879974151586167e-13, 2.0436130804366296e-14, 6.18907139084941e-16, 9.14641092243755e-18]
+
+DEFAULT_TOP_K = 10
 AUTHOR_EVERYONE = "<everyone>"
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 #####################
 
-PROJECT_DIR = input(f"absolute path of git repo (directory) to check \x1b[33m(leave empty to use {THIS_DIR})\x1b[0m: ")
+def main():
+	project_dir = input(f"absolute path of git repo (directory) to check \x1b[33m(leave empty to use {THIS_DIR})\x1b[0m: ").strip()
+	project_dir = project_dir if project_dir != "" else THIS_DIR
+	if not os.path.isdir(project_dir):
+		print(f"\x1b[31merror: invalid directory \"{project_dir}\"\x1b[0m")
+		exit(1)
+	print(f"  checking directory: \x1b[35m{project_dir}\x1b[0m!")
 
-if PROJECT_DIR == "": # use dir of this file
-	PROJECT_DIR = THIS_DIR
-elif not os.path.isdir(PROJECT_DIR):
-	print("\x1b[31mInvalid directory\x1b[0m")
-	exit(1)
-print(f"Checking directory: \x1b[35m{PROJECT_DIR}\x1b[0m")
+	commit_author = input("which commit author or git username? \x1b[33m(leave empty for everyone)\x1b[0m: ").strip()
+	commit_author = commit_author if commit_author != "" else AUTHOR_EVERYONE
+	print(f"  checking commits by: \x1b[35m{commit_author}\x1b[0m!")
 
-COMMIT_AUTHOR = input("which commit author or git username? \x1b[33m(leave empty for everyone)\x1b[0m: ")
-COMMIT_AUTHOR = COMMIT_AUTHOR if COMMIT_AUTHOR != "" else AUTHOR_EVERYONE
-print(f"Checking commits by: \x1b[35m{COMMIT_AUTHOR}\x1b[0m")
+	top_k = input(f"get the top how many hashes? \x1b[33m(leave empty for {DEFAULT_TOP_K})\x1b[0m: ").strip()
+	if top_k != "":
+		try:
+			top_k = int(top_k)
+		except ValueError:
+			top_k = DEFAULT_TOP_K
+			print(f">>> invalid number provided, using default...")
+	else:
+		top_k = DEFAULT_TOP_K
+	
+	print(f">>> fetching top \x1b[35m{top_k}\x1b[0m hashes with most letters/numbers\x1b[0m!")
 
-all_shas = {}
-os.chdir(PROJECT_DIR)
+	all_shas = {}
+	os.chdir(project_dir)
 
-shas = ...
-try:
-	subp_cmd = ['git', 'log', '--pretty=format:%H', '--all']
-	if COMMIT_AUTHOR != AUTHOR_EVERYONE:
-		subp_cmd.insert(2, f'--author={COMMIT_AUTHOR}')
+	shas = ...
+	try:
+		subp_cmd = ['git', 'log', '--pretty=format:%H', '--all']
+		if commit_author != AUTHOR_EVERYONE:
+			subp_cmd.insert(2, f'--author=\"{commit_author}\"')
 
-	shas = subprocess.check_output(
-		subp_cmd,
-		stderr=subprocess.DEVNULL,
-		shell=True
-	)
-except subprocess.CalledProcessError as e:
-	print(f"\x1b[31mError: {e}\x1b[0m")
-	exit(1)
+		shas = subprocess.check_output(
+			subp_cmd,
+			stderr=subprocess.DEVNULL,
+			shell=True
+		)
+	except subprocess.CalledProcessError as e:
+		print(f"\x1b[31merror: {e}\x1b[0m")
+		exit(1)
 
-shas_list = shas.decode('utf-8').split('\n')
-all_shas.update({(hsh, PROJECT_DIR) for hsh in shas_list})
+	shas_list = shas.decode('utf-8').split('\n')
 
-print(f"\ntotal UNIQUE commits by \x1b[35m{COMMIT_AUTHOR}\x1b[0m in {PROJECT_DIR}: {len(all_shas)}")
+	# maybe in the future can support multiple subdirectories being repos
+	# originally thats what this was for which is why project_dir is stored here
+	all_shas.update({(hsh, project_dir) for hsh in shas_list})
 
-print()
 
-most_letters_hash = None
-most_letters_hash_num = 0
-most_numbers_hash = None
-most_numbers_hash_num = 0
+	if len(all_shas) < 1 or "" in all_shas:
+		# probably means no commits found due to invalid/nonexistent author
+		print(f"\n>>> no commits found by \x1b[33m{commit_author}\x1b[0m in \x1b[33m{project_dir}\x1b[0m!")
+		exit(0)
 
-for hsh in all_shas:
-	letters = sum(c.isalpha() for c in hsh)
-	numbers = sum(c.isdigit() for c in hsh)
+	print(f"\n>>> total unique commits by \x1b[33m{commit_author}\x1b[0m in \x1b[33m{project_dir}\x1b[0m: {len(all_shas)}")
 
-	if letters > most_letters_hash_num:
-		most_letters_hash_num = letters
-		most_letters_hash = hsh
+	topk_letters = [] # will store tuples of (hash, letters)
+	topk_numbers = [] # will store tuples of (hash, numbers)
 
-	if numbers > most_numbers_hash_num:
-		most_numbers_hash_num = numbers
-		most_numbers_hash = hsh
+	# current minimum "specialness" to qualify for the leaderboards
+	# lazy - we will sort and take the top k at the end
+	curr_min_letters = 0
+	curr_min_numbers = 0
 
-most_letters_prob = PROBS[most_letters_hash_num]
-most_numbers_prob = 1 - PROBS[40 - most_numbers_hash_num]
+	for hsh in all_shas:
+		letters_ct = sum(c.isalpha() for c in hsh)
+		numbers_ct = 40 - letters_ct
 
-info_for_most_letters = get_commit_info(most_letters_hash)
-info_for_most_numbers = get_commit_info(most_numbers_hash)
+		if letters_ct >= curr_min_letters:
+			topk_letters.append((hsh, letters_ct))
+			if len(topk_letters) >= top_k: # leaderboard getting too full raise the bar!!!!!!
+				curr_min_letters = min(topk_letters, key=lambda x: x[1])[1]
 
-print(f"Most letters: {most_letters_hash_num}/40 - \x1b[34m{most_letters_hash}\x1b[0m\x1b[33m ({most_letters_prob*100:_.12f}%)\x1b[0m")
-print(f"\tby \x1b[33m{info_for_most_letters['author']}\x1b[0m on \x1b[33m{info_for_most_letters['date']}\x1b[0m")
-# print(f"\"{info_for_most_letters['message']}\"")
+		if numbers_ct >= curr_min_numbers:
+			topk_numbers.append((hsh, numbers_ct))
+			if len(topk_numbers) >= top_k:
+				curr_min_numbers = min(topk_numbers, key=lambda x: x[1])[1]
 
-print()
+	# prune leaderboards to get top k
+	topk_letters.sort(key=lambda x: x[1], reverse=True)
+	real_topk_letters = topk_letters[:top_k]
 
-print(f"Most numbers: {most_numbers_hash_num}/40 - \x1b[34m{most_numbers_hash}\x1b[0m\x1b[33m ({most_numbers_prob*100:_.12f}%)\x1b[0m")
-print(f"\tby \x1b[33m{info_for_most_numbers['author']}\x1b[0m on \x1b[33m{info_for_most_numbers['date']}\x1b[0m")
-#print(f"\"{info_for_most_numbers['message']}\"")
+	topk_numbers.sort(key=lambda x: x[1], reverse=True)
+	real_topk_numbers = topk_numbers[:top_k]
 
-print()
+	print(f"\ntop {top_k} most letters:")
+	for i in range(len(real_topk_letters)):
+		hsh, n_letters = real_topk_letters[i]
+		infos = get_commit_info(hsh)
+		prob = PROBS[n_letters]
+		print(f"#{i+1}: \x1b[34m{hsh}\x1b[0m - {n_letters} letters ({prob*100:_.7f}%)\x1b[0m")
+		print(f"  by \x1b[33m{infos['author']}\x1b[0m on \x1b[33m{infos['date']}\x1b[0m")
+		print(f"  \"{ellipsisize_text(infos['message'], 50)}\"")
 
-print(f"probabilities calculated for x or more letters/numbers.")
-print(f"run \x1b[33mgit show --format=%B -s \x1b[34m<commit-hash>\x1b[0m to check commit messages!")
+	print(f"\ntop {top_k} most numbers:")
+	for i in range(len(real_topk_numbers)):
+		hsh, n_numbers = real_topk_numbers[i]
+		infos = get_commit_info(hsh)
+		prob = 1 - PROBS[40 - n_numbers]
+		print(f"#{i+1}: \x1b[34m{hsh}\x1b[0m - {n_numbers} numbers ({prob*100:_.7f}%)\x1b[0m")
+		print(f"  by \x1b[33m{infos['author']}\x1b[0m on \x1b[33m{infos['date']}\x1b[0m")
+		print(f"  \"{ellipsisize_text(infos['message'], 50)}\"")
+
+	print(f"\n\x1b[2mprobabilities calculated for x or more letters/numbers.")
+	print(f"\x1b[2mrun 'git show --format=%B -s <commit-hash>' to check commit messages!\x1b[0m")
+
+if __name__ == "__main__":
+	main()
